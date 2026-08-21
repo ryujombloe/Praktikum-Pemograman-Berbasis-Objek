386d56ca21af2ab830bd54e3ff404b8a8384efa4
ryujombloe <nacu20045@gmail.com>
2026-08-21 16:54:43 +0700

    Memperbaiki struktur Proyek 1

---
+++ Muhammad_Naveed_Sharaheel/project_1/src/main/java/com/pbo/project_1/Biodata.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.pbo.project_1;
+
+/**
+ *
+ * @author Ryuugay
+ */
+public class Biodata {
+    public static void main(String[] args) {
+        
+        System.out.println("Nama   : Muhammad Naveed Sharaheel");
+        System.out.println("NPM    : 2320506040");
+        System.out.println("Alamat : Magelang, Jawa Tengah");
+        System.out.println("No_HP  : 082********");
+        
+    }
+}
---
+++ Muhammad_Naveed_Sharaheel/project_1/src/main/java/com/pbo/project_1/Project_1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.pbo.project_1;
+
+/**
+ *
+ * @author Ryuugay
+ */
+public class Project_1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
